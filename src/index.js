1e6496f15ba16203ca4c6367dbf6b3ac0d6e1eaf
Alejo2608 <bricenovalecillose@uvm.edu.ve>
2022-08-02 16:59:28 -0400

    Cambios

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../src/redux/store/index.js';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
