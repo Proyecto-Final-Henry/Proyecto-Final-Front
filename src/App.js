0957dee22e86b844ae7f0fdc988ccb4dda678b9b
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-02 15:27:06 -0300

    Componentes premium y contacto (esqueleto)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import { Route } from "react-router-dom";
 import Home from "./components/Home/Home"
 import Login from "./components/Login/Login"
+import Contact from "./components/Contact/Contact"
+import Premium from "./components/Premium/Premium"
 import './App.css';
 
 
@@ -11,6 +13,8 @@ function App() {
     <div className="App">
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login}/>
+      <Route path="/premium" component={Premium}/>
+      <Route path="/contact" component={Contact}/>
     </div>
 
   );
