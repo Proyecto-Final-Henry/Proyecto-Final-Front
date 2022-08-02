029392e45aa1311cea141b61334c82593bfede97
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-02 20:38:15 -0300

    Register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,20 @@
 function Register() {
     return (
-      <div>
-        <h1>aqui va el Register</h1>
-      </div>
+      <form>
+        <div class="form-group">
+          <label style={{color: "white"}}>Nombre</label>
+          <input type="text" class="form-control" name="InputName" aria-describedby="emailHelp" placeholder="Nombre..."/>
+        </div>
+        <div class="form-group">
+          <label style={{color: "white"}} >Direccion de email</label>
+          <input type="email" class="form-control" name="InputEmail" aria-describedby="emailHelp" placeholder="Correo..."/>
+        </div>
+        <div class="form-group">
+          <label style={{color: "white"}}>Contraseña</label>
+          <input type="password" class="form-control" name="InputPassword" placeholder="Contraseña..."/>
+        </div>
+        <button type="submit" class="btn btn-primary">Registrarse</button>
+      </form>
     );
   }
 
