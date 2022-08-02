7d9fd2667010bff1504acb58207db770de87f530
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-02 15:49:35 -0300

    Componentes premium y contacto

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,19 +2,25 @@ import Nav from "../Nav/Nav"
 
 
 function Contact() {
+
+    const style = {
+        color: 'white',
+      };
+
+
     return (
       <div>
         <Nav/>
-        <div>
+        <div style={style}>
            <h1>Información de contacto</h1>          
         </div>
-        
-        <div>
+
+        <div style={style}>
             <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, veritatis harum! Animi molestias, incidunt dolor soluta est voluptas, quod temporibus obcaecati sit quam dignissimos officiis accusantium ipsa iure impedit reiciendis ad molestiae praesentium porro quia voluptatum dolorem? Assumenda nam odit eos! Maxime, ipsam enim veniam animi sunt nesciunt quibusdam porro!</h3>
             <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est laborum velit ut asperiores explicabo fugit totam distinctio voluptatibus sequi corporis. Quia nemo eum corporis dignissimos animi consectetur officia provident. Quam quidem adipisci sint commodi ducimus inventore praesentium voluptates nam facilis.</h3>
         </div>
 
-        <div>
+        <div style={style}>
             <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque maxime quae laborum consequuntur repellendus, soluta velit possimus non voluptates vitae itaque eaque molestias reprehenderit ea illo assumenda quibusdam illum blanditiis?</h4>
             <h4>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut impedit, dolorem nulla nesciunt eos placeat ex debitis, totam ab, facere velit pariatur assumenda veritatis omnis.</h4>
         </div>
