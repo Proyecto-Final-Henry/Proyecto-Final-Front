41046626d659e8a088deb5cb2ddae40c8da6c4e7
Julian Lechuga <julianlechuga12@gmail.com>
2022-08-03 19:41:35 -0300

    Seteo de componenetes y prototipo de nav

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,8 @@
 export default function Feed(){
     return(
         <div>
-            aqui va el feed
+            <br />
+            <img src="https://viapais.com.ar/resizer/XH4OAJCYd2SWrQArvlIU84i9ERU=/982x551/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/7XKUVIER6JFZDGODBA42HUFTBQ.jpg" alt="aca va el feed" />
         </div>
     )
 }
