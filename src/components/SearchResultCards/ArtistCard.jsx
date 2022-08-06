ae67286dac3931479b5ea8f9d0abe65cb8af9627
lal-fac <luis.alfonso.ep@gmail.com>
2022-08-06 14:29:14 -0500

    feat(SearchResult Links to Artist & Album)

--- src/components/SearchResultCards/ArtistCard.jsx
+++ src/components/SearchResultCards/ArtistCard.jsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 import style from "../../css/resultSearch.module.css";
 
-export default function ArtistCard (props){
-    return(
-        <div className={style.card}>
-            <img src={props.img} alt="img"></img>
-            <div className={style.text}>
-            <p>{props.name}</p>
-            <p>{props.type}</p>
-            </div>                       
-        </div>
-    );
-};
+export default function ArtistCard(props) {
+  return (
+    <div className={style.card}>
+      <img src={props.img} alt="img"></img>
+      <div className={style.text}>
+        <Link to={`/artist/${props.id}`}>
+          <p>{props.name}</p>
+        </Link>
+        <p>{props.type}</p>
+      </div>
+    </div>
+  );
+}
