7d24a547226f0425d5167ce7808f0dfa3565ee63
Alejo2608 <bricenovalecillose@uvm.edu.ve>
2022-08-13 20:07:52 -0400

    arreglo

--- src/components/Socket/Socket.jsx
+++ src/components/Socket/Socket.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import io from 'socket.io-client';
-import {getAllUsers} from "../../redux/actions/index.js"
-import { useSelector,useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+// import React from 'react';
+// import io from 'socket.io-client';
+// import {getAllUsers} from "../../redux/actions/index.js"
+// import { useSelector,useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
 
-export default function Socket(){
-    let dispatch = useDispatch();
-    let users= useSelector((state) => state.users);
-    console.log(users)
+// export default function Socket(){
+//     let dispatch = useDispatch();
+//     let users= useSelector((state) => state.users);
+//     console.log(users)
 
-    useEffect(() => {
-        try {
-            const socket=io('http://localhost:3001')
-            console.log(socket.on('firstEvent', (msg)=>{
-                console.log(msg)
-            }))
-        } catch (err) {
-            console.log(err)
-        }
-        dispatch(getAllUsers())
-    },[]);
+//     useEffect(() => {
+//         try {
+//             const socket=io('http://localhost:3001')
+//             console.log(socket.on('firstEvent', (msg)=>{
+//                 console.log(msg)
+//             }))
+//         } catch (err) {
+//             console.log(err)
+//         }
+//         dispatch(getAllUsers())
+//     },[]);
 
 
 
-    return(
-        <div className="noti">
-            <h1>Noti</h1>
-        </div>
-    )
-}
+//     return(
+//         <div className="noti">
+//             <h1>Noti</h1>
+//         </div>
+//     )
+// }
