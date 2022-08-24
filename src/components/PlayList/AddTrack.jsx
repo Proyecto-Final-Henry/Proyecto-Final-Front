ea269ac66503f6ae3efdc179ddb2fcfec2d88b9f
cristhian alban <djstylecali@gmail.com>
2022-08-23 20:13:19 -0500

    fix bug css boton add playlist y selectors searchbar

--- src/components/PlayList/AddTrack.jsx
+++ src/components/PlayList/AddTrack.jsx
@@ -22,7 +22,7 @@ export default function AddTrack({userId, trackId, name}) {
                 userId={userId}
                 />
             </Modal>
-            <button className="follow" onClick={openModal}>+/- Playlist</button>
+            <button className="addplaylist" onClick={openModal}>+/- Playlist</button>
         </div>
     )
 }
