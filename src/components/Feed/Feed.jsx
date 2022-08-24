6c97de7b48cd9c37d2742852d71157f6f4bc48fd
Alejo2608 <bricenovalecillose@uvm.edu.ve>
2022-08-23 23:01:53 -0400

    cam

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,7 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { useEffect, useRef  } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import Per from './PerfilSide';
 import ReviewCard from "../ReviewCard/ReviewCard";
