d60d3bf890a789f1bc2c61ebea7fd731346b17bd
lal-fac <luis.alfonso.ep@gmail.com>
2022-08-24 20:58:48 -0500

    bug(playlist en userProfile)

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -4,13 +4,13 @@ import { useHistory, Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
 import "../../css/users.css";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
 import { useDispatch } from "react-redux";
 import ChangeProfileImg from "../ChangeProfileImg/ChangeProfileImg";
 import Spinner from "react-bootstrap/Spinner";
 import MyReview from "./MyReview";
 import loading from "../../assets/loading.gif";
-import MyPlaylist from "./MyPlaylist.jsx"
+import MyPlaylist from "./MyPlaylist.jsx";
 
 export default function UserProfile() {
   //const data =useSelector(store => store.userData) // descomentar para subcribir el componete al stado global con la data que se pide por params
@@ -34,17 +34,16 @@ export default function UserProfile() {
       },
     };
     try {
-        const { data } = await axios.post(
-          `/api/back-end/users/create_preference`,
-          { description: "Premium", price: 1, quantity: 1 },
-          config
-        );
-        window.open(data.id.sandbox_init_point); // sandbox_init_point
+      const { data } = await axios.post(
+        `/api/back-end/users/create_preference`,
+        { description: "Premium", price: 1, quantity: 1 },
+        config
+      );
+      window.open(data.id.sandbox_init_point); // sandbox_init_point
     } catch (error) {
       console.log(error);
-    };
+    }
   };
-
 
   const handleShowImg = (e) => {
     e.preventDefault();
@@ -81,7 +80,7 @@ export default function UserProfile() {
     };
     autenticarUsuario();
   }, [showImg]);
-  console.log(user)
+  console.log(user);
 
   const cerrarSesion = () => {
     localStorage.removeItem("token");
@@ -90,73 +89,163 @@ export default function UserProfile() {
   };
 
   return (
-    <div  className="pepe" style={{position: "relative"}}>
-      <div className="modal fade" id="MercadoModal" tabIndex="-1" role="dialog" aria-labelledby="MercadoModalLabel" aria-hidden="true">
-                            <div className="modal-dialog" role="document">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h5 className="modal-title" id="MercadoModalLabel">Pago Premium</h5>
-                                        <button type="button" className="btn btn-outline-secondary" data-dismiss="modal" aria-label="Close"> X </button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <h4>
-                                            Seras redirigido a MercadoPago
-                                        </h4>
-                                    </div>
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-outline-success" data-dismiss="modal" onClick={handleButton} data-toggle="modal" data-target="#Mercado2Modal">Ok</button>
-                                        <button type="button" className="btn btn-outline-danger" data-dismiss="modal">Cerrar</button>
-                                    </div>
-                                </div>
-                            </div>
-                     </div>
-
-                     <div className="modal fade" id="Mercado2Modal" tabIndex="-1" role="dialog" aria-labelledby="MercadoModalLabel" aria-hidden="true">
-                            <div className="modal-dialog" role="document" >
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h5 className="modal-title" id="MercadoModalLabel">Pago Premium</h5>
-                                        <button type="button" className="btn btn-outline-secondary" data-dismiss="modal" aria-label="Close"> X </button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <h4>
-                                            Procesando tu pago...
-                                        </h4>
-                                        <img style={{heigth:"250px", width:"350px"}} src={loading} alt="cargando..." />
-                                    </div>
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-outline-danger" data-dismiss="modal">Cerrar</button>
-                                    </div>
-                                </div>
-                            </div>
-                     </div>
-    <div className="carda">
-      <div className="img">
-        <img src={user?.userImg} alt="userImg"></img>
+    <div className="pepe" style={{ position: "relative" }}>
+      <div
+        className="modal fade"
+        id="MercadoModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="MercadoModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="MercadoModalLabel">
+                Pago Premium
+              </h5>
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                {" "}
+                X{" "}
+              </button>
+            </div>
+            <div className="modal-body">
+              <h4>Seras redirigido a MercadoPago</h4>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-outline-success"
+                data-dismiss="modal"
+                onClick={handleButton}
+                data-toggle="modal"
+                data-target="#Mercado2Modal"
+              >
+                Ok
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-danger"
+                data-dismiss="modal"
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="content">
-        <h3>{user?.name}</h3>
+
+      <div
+        className="modal fade"
+        id="Mercado2Modal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="MercadoModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="MercadoModalLabel">
+                Pago Premium
+              </h5>
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                {" "}
+                X{" "}
+              </button>
+            </div>
+            <div className="modal-body">
+              <h4>Procesando tu pago...</h4>
+              <img
+                style={{ heigth: "250px", width: "350px" }}
+                src={loading}
+                alt="cargando..."
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-outline-danger"
+                data-dismiss="modal"
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="carda">
+        {/* <div className="img">
+          <img src={user?.userImg} alt="userImg"></img>
+        </div> */}
+        {showImg ? (
+          <div className="hov">
+            <div className="pri">
+              <div className="img">
+                <img src={user?.userImg} alt="userImg"></img>
+              </div>
+              <button onClick={handleShowImg} className="bo">
+                📸
+              </button>
+            </div>
+          </div>
+        ) : (
+          <ChangeProfileImg userId={user.id} setShowImg={setShowImg} />
+        )}
+        <div className="content">
+          <h3>{user?.name}</h3>
           <p>{user?.email}</p>
           <p>Usuario {user?.role}</p>
-          <p >Miembro desde {user?.createdDate}</p>
+          <p>Miembro desde {user?.createdDate}</p>
           <div className="center">
-              <div className="box">
-                <p>{user?.followers?.length}</p>
-                <p className="userP">Seguidores</p>
-              </div>
-              <div className="box">
-                <p>{user?.following?.length}</p>
-                <p className="userP">Seguidos</p>
-              </div>
+            <div className="box">
+              <p>{user?.followers?.length}</p>
+              <p className="userP">Seguidores</p>
+            </div>
+            <div className="box">
+              <p>{user?.following?.length}</p>
+              <p className="userP">Seguidos</p>
+            </div>
+          </div>
+          <br />
+          <Nav className="nav_btn_registro">
+            <button
+              style={{ marginTop: "6px", color: "white" }}
+              className="btn_registrate"
+              onClick={cerrarSesion}
+              variant="outline-danger"
+              type="submit"
+            >
+              Cerrar Sesión
+            </button>
+          </Nav>
+          <br />
+          {user.role === "Gratuito" ? (
+            <span>
+              <button
+                type="button"
+                className="btn btn-outline-success"
+                data-toggle="modal"
+                data-target="#MercadoModal"
+              >
+                {" "}
+                Cambiar a plan Premium{" "}
+              </button>{" "}
+              <br />{" "}
+            </span>
+          ) : null}
         </div>
-        <br />
-        <Nav className='nav_btn_registro'>
-            <button style={{marginTop: "6px", "color":"white"}} className="btn_registrate" onClick={cerrarSesion} variant="outline-danger" type="submit">Cerrar Sesión</button>
-        </Nav> 
-        <br />
-        {user.role === "Gratuito" ? <span><button type="button" className="btn btn-outline-success" data-toggle="modal" data-target="#MercadoModal"> Cambiar a plan Premium </button> <br /> </span> : null}
       </div>
-    </div>
 
       {/* <div className="detailBac">
         <div className="detail">
@@ -247,9 +336,7 @@ export default function UserProfile() {
         )}
       </div> */}
       <MyReview />
-      <div className="play">
-        <MyPlaylist userId={user.id}/>
-      </div>
+      <div className="play">{user.id && <MyPlaylist userId={user.id} />}</div>
     </div>
   );
-};
+}
